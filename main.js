c6c47e1ba83e313675c1e37b530801a89d533fed
ntownse2 <nicholas.m.townsend@gmail.com>
2014-08-12 11:55:06 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var red = [0,100, 63];
+var purple = [280, 50, 60];
+var myName = "Tess";
+var letterColors = [red, purple];
+
+if (myName) {
+    bubbleShape = "circle";
+    } else{
+      bubbleShape = "circle";
+      }:
+
+drawName (myName, letterColors);
+bounceBubbles();
